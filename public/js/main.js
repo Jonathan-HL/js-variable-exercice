62d5233908630224f10d1691ada1cb8ad817c99d
Jonathan <jonxplod@outlook.fr>
2021-10-04 22:11:13 +0200

    exercice 5

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+// Exo 1
+let chiffre = 1
+let chiffre2 = 5
+let calcule = `${chiffre} + ${chiffre2} = ${chiffre + chiffre2}`
+let calcule2 = `${chiffre2} - ${chiffre} = ${chiffre2 - chiffre}`
+let calcule3 = `${chiffre2} * ${chiffre} = ${chiffre2 * chiffre}`
+let calcule4 = `${chiffre2} / ${chiffre} = ${chiffre2 / chiffre}`
+console.log(calcule);
+console.log(calcule2);
+console.log(calcule3);
+console.log(calcule4);
+//Exo 2
+let age = Number(prompt("Ton age ?"));
+let prenom = prompt("Ton prénom ?");
+console.log(`${prenom} a ${age} ans cette année`);
+console.log(`${prenom} a ${age+3} ans dans 3 ans`);
+//Exo 3
+let anne = `1996`
+let anneActuelle = `2021`
+console.log(anneActuelle - anne);
+console.log((anneActuelle - anne) +2);
